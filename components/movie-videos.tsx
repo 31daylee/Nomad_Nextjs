8cfac3affa6eb3374de50d04404d87abe592f08a
31daylee <gussla2206@gmail.com>
2024-06-13 17:00:31 +0900

    3.7 Error Handling

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,10 +1,11 @@
 import { API_URL } from "../app/(home)/page";
 
 async function getMovie(id: string) {
-  //console.log(`Fetching movies: ${Date.now()}`);
-  // await new Promise((resolve) => setTimeout(resolve, 2000));
-  const response = await fetch(`${API_URL}/${id}`);
-  return response.json();
+  console.log(`Fetching movies: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  throw new Error("something broke...");
+  //   const response = await fetch(`${API_URL}/${id}`);
+  //   return response.json();
 }
 
 export default async function MovieInfo({ id }: { id: string }) {
