dc78e623f28d3cacc92bf54e3536e5d8fe51b5a8
31daylee <gussla2206@gmail.com>
2024-06-26 15:40:14 +0900

    movie-info-nav active success

--- components/movie-info-nav.tsx
+++ components/movie-info-nav.tsx
@@ -1,35 +1,54 @@
 "use client";
 
 import Link from "next/link";
+import { useState } from "react";
 import styles from "../styles/movie-info-nav.module.css";
 import { useViewStore } from "../stores/viewStore";
 
-export default function Navigation({ id }: { id: string }) {
+export default function Navigation() {
   const setView = useViewStore((state) => state.setView);
+  const [activeView, setActiveView] = useState<string>("overview");
 
   const handleClick = (view: string) => {
     setView(view);
+    setActiveView(view);
   };
 
   return (
     <nav className={styles.nav}>
       <ul>
-        <li>
+        <li
+          className={`${styles.navItem} ${
+            activeView === "overview" ? styles.active : ""
+          }`}
+        >
           <Link href="#" onClick={() => handleClick("overview")}>
             OVERVIEW
           </Link>
         </li>
-        <li>
+        <li
+          className={`${styles.navItem} ${
+            activeView === "videos" ? styles.active : ""
+          }`}
+        >
           <Link href="#" onClick={() => handleClick("videos")}>
             VIDEOS
           </Link>
         </li>
-        <li>
+        <li
+          className={`${styles.navItem} ${
+            activeView === "series" ? styles.active : ""
+          }`}
+        >
           <Link href="#" onClick={() => handleClick("series")}>
             SERIES
           </Link>
         </li>
-        <li>
+        <li
+          className={`${styles.navItem} ${
+            activeView === "review" ? styles.active : ""
+          }`}
+        >
           <Link href="#" onClick={() => handleClick("review")}>
             REVIEW
           </Link>
